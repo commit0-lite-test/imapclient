5602f21c5c814bc96c63885b0d8d267cfc57ffe8
nanjiangwill <willjiang2018@gmail.com>
2024-11-15 13:17:39 -0800

    fix: Normalize datetime in parse_to_datetime function

--- imapclient/datetime_util.py
+++ imapclient/datetime_util.py
@@ -2,9 +2,11 @@ import re
 from datetime import datetime
 from email.utils import parsedate_tz
 from .fixed_offset import FixedOffset
-_SHORT_MONTHS = ' Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec'.split(' ')
 
-def parse_to_datetime(timestamp: bytes, normalise: bool=True) -> datetime:
+_SHORT_MONTHS = " Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec".split(" ")
+
+
+def parse_to_datetime(timestamp: bytes, normalise: bool = True) -> datetime:
     """Convert an IMAP datetime string to a datetime.
 
     If normalise is True (the default), then the returned datetime
@@ -13,20 +15,21 @@ def parse_to_datetime(timestamp: bytes, normalise: bool=True) -> datetime:
     If normalise is False, then the returned datetime will be
     unadjusted but will contain timezone information as per the input.
     """
-    timestamp_str = timestamp.decode('ascii')
+    timestamp_str = timestamp.decode("ascii")
     parsed = parsedate_tz(timestamp_str)
     if parsed is None:
         raise ValueError(f"Could not parse datetime string: {timestamp_str}")
-    
+
     dt = datetime(*parsed[:6])
     offset_seconds = parsed[-1]
-    
+
     if offset_seconds is not None:
         dt = dt.replace(tzinfo=FixedOffset(offset_seconds // 60))
-    
+
     if normalise:
         return dt.astimezone().replace(tzinfo=None)
     return dt
+
 
 def datetime_to_INTERNALDATE(dt: datetime) -> str:
     """Convert a datetime instance to a IMAP INTERNALDATE string.
@@ -36,12 +39,17 @@ def datetime_to_INTERNALDATE(dt: datetime) -> str:
     """
     if dt.tzinfo is None:
         dt = dt.replace(tzinfo=FixedOffset.for_system())
-    
+
     return dt.strftime("%d-%b-%Y %H:%M:%S %z")
-_rfc822_dotted_time = re.compile('\\w+, ?\\d{1,2} \\w+ \\d\\d(\\d\\d)? \\d\\d?\\.\\d\\d?\\.\\d\\d?.*')
+
+
+_rfc822_dotted_time = re.compile(
+    "\\w+, ?\\d{1,2} \\w+ \\d\\d(\\d\\d)? \\d\\d?\\.\\d\\d?\\.\\d\\d?.*"
+)
+
 
 def format_criteria_date(dt: datetime) -> bytes:
     """Format a date or datetime instance for use in IMAP search criteria."""
     if isinstance(dt, datetime):
         dt = dt.date()
-    return dt.strftime("%d-%b-%Y").encode('ascii')
+    return dt.strftime("%d-%b-%Y").encode("ascii")
