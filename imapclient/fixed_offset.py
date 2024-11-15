8026a573ed9c5afe689ce547ca38babf1f4e2822
nanjiangwill <willjiang2018@gmail.com>
2024-11-15 13:18:26 -0800

    fix: Improve formatting and docstring in fixed_offset.py

--- imapclient/fixed_offset.py
+++ imapclient/fixed_offset.py
@@ -1,21 +1,22 @@
 import datetime
 import time
 from typing import Optional
+
 ZERO = datetime.timedelta(0)
 
+
 class FixedOffset(datetime.tzinfo):
-    """
-    This class describes fixed timezone offsets in hours and minutes
+    """This class describes fixed timezone offsets in hours and minutes
     east from UTC
     """
 
     def __init__(self, minutes: float) -> None:
         self.__offset = datetime.timedelta(minutes=minutes)
-        sign = '+'
+        sign = "+"
         if minutes < 0:
-            sign = '-'
+            sign = "-"
         hours, remaining_mins = divmod(abs(minutes), 60)
-        self.__name = '%s%02d%02d' % (sign, hours, remaining_mins)
+        self.__name = "%s%02d%02d" % (sign, hours, remaining_mins)
 
     def utcoffset(self, dt: Optional[datetime.datetime]) -> datetime.timedelta:
         return self.__offset
@@ -27,7 +28,7 @@ class FixedOffset(datetime.tzinfo):
         return ZERO
 
     @classmethod
-    def for_system(cls) -> 'FixedOffset':
+    def for_system(cls) -> "FixedOffset":
         """Return a FixedOffset instance for the current working timezone and
         DST conditions.
         """
